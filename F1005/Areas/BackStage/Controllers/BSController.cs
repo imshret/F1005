b038dc8bce98e46b5c5b3ffc7f38cde123a3bfe9
imshret <frompsc@gmail.com>
2018-09-29 10:57:28 +0800

    Update

---
+++ F1005/Areas/BackStage/Controllers/BSController.cs
@@ -0,0 +1,28 @@
+﻿using F1005.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace F1005.Areas.BackStage.Controllers
+{
+    public class BSController : Controller
+    {
+        // GET: BackStage/BS
+        public ActionResult IndexStock()
+        {
+            MyInvestEntities db = new MyInvestEntities();
+                       
+            return View(db.StockHistory.ToList());
+        }
+
+        public ActionResult IndexFX()
+        {
+            MyInvestEntities db = new MyInvestEntities();
+
+            return View(db.FXtradeTable.ToList());
+        }
+
+    }
+}
