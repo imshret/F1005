2f8e39ca1c963ea3164d9e34463c9dd163e645ba
Wayne <40432032+JWayneLink@users.noreply.github.com>
2018-10-01 14:17:45 +0800

    Cash+Main+NuGet+ RefreshModel
    Cash+Main+NuGet+ RefreshModel

--- F1005/Areas/Cash/Controllers/GetExpenseViewModel.cs
+++ F1005/Areas/Cash/Controllers/GetExpenseViewModel.cs
@@ -4,18 +4,9 @@
     {
         public int ExCashID { get; set; }
         public string UserID { get; set; }
-        public ExpenseType ExCashType { get; set; }
+        public string ExCashType { get; set; }
         public int? ExAmount { get; set; }
         public string ExDate { get; set; }
         public string ExNote { get; set; }
-    }
-
-    public enum ExpenseType
-    {
-        現金 = 1,
-        股票 = 2,
-        外匯 = 3,
-        基金 = 4,
-        儲蓄險 = 5
     }
 }
